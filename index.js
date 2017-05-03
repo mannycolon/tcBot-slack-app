970316d03117e7344ea9fbdee8d4e9dade2665ce
mannycolon <colonmanuel7@gmail.com>
2017-05-03 00:51:40 -0400

    created node app with mongodb

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict'
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+// Mongoose Schema definition
+var Schema = new mongoose.Schema({
+  user     : String,
+  task     : String,
+  timestamp: String,
+  available: Boolean
+});
+
+var Users = mongoose.model('Users', Schema);
+
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+express()
+  .use(bodyParser.json()) // support json encoded bodies
+  .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+
+  .post('/', function (req, res) {
+    var todo = new Users( req.body );
+    todo.id = todo._id;
+    console.log(todo)
+    // http://mongoosejs.com/docs/api.html#model_Model-save
+    todo.save(function (err) {
+      res.json(200, todo);
+    });
+    console.log(todo)
+  })
+  // .get('/', (req, res) => {
+  //   handleQueries(req.query, res);
+  // })
+
+  // .post('/', (req, res) => {
+  //   handleQueries(req.body, res);
+  // })
+
+  .listen(process.env.PORT || 5000, () => {
+    console.log('Expresss server listening on port ' + process.env.PORT || 5000)
+  });
+
+
