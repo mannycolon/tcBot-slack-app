8ec38fd1a08ce449bd428248b1fb22d1a882b6f6
mannycolon <colonmanuel7@gmail.com>
2017-05-05 01:02:12 -0400

    made a function to hanlde parsing text from the server request body

--- index.js
+++ index.js
@@ -3,36 +3,37 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-var url = 'mongodb://heroku_1rfb6cx7:csft257r52q2mqtq41fdpt2dg6@ds129641.mlab.com:29641/heroku_1rfb6cx7'
+let url = 'mongodb://heroku_1rfb6cx7:csft257r52q2mqtq41fdpt2dg6@ds129641.mlab.com:29641/heroku_1rfb6cx7'
 
 // Mongoose Schema definition
-var Schema = new mongoose.Schema({
+let Schema = new mongoose.Schema({
   user     : String,
   task     : String,
   timestamp: String
 });
 
-var Users = mongoose.model('Users', Schema);
+let Users = mongoose.model('Users', Schema);
 
 mongoose.connect(url, function (error) {
     if (error) console.error(error);
     else console.log('mongo connected');
 });
 
+function textParser(text, identifier) {
+  return text.split(' ').filter((element) => {
+    return element.includes(identifier)
+  })
+}
+
 express()
   .use(bodyParser.json()) // support json encoded bodies
   .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
   .post('/', function (req, res) {
-    var todo = new Users( req.body );
+    let todo = new Users( req.body );
 
-    let user = req.body.text.split(' ').filter((element) => {
-      return element.includes('@')
-    })
-
-    let task = req.body.text.split(' ').filter((element) => {
-      return element.includes('#')
-    })
+    let user = textParser(req.body.text, '@');
+    let task = textParser(req.body.text, '#');
 
     todo.id = todo._id;
     todo.user = user;
@@ -53,16 +54,8 @@ express()
     });
     console.log(todo)
   })
-  // .get('/', (req, res) => {
-  //   handleQueries(req.query, res);
-  // })
-
-  // .post('/', (req, res) => {
-  //   handleQueries(req.body, res);
-  // })
 
   .listen(process.env.PORT || 5000, () => {
     console.log('Expresss server listening on port ' + process.env.PORT || 5000)
   });
-
 
