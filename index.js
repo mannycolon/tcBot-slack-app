0b69901c6887a9edbc8658bb7e4985c20d25fe7e
mannycolon <colonmanuel7@gmail.com>
2017-05-05 02:17:50 -0400

    rework the server

--- index.js
+++ index.js
@@ -2,22 +2,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
-let url = 'mongodb://heroku_1rfb6cx7:csft257r52q2mqtq41fdpt2dg6@ds129641.mlab.com:29641/heroku_1rfb6cx7'
-
-// Mongoose Schema definition
-let Schema = new mongoose.Schema({
-  user     : String,
-  task     : String,
-  timestamp: String
-});
-
-let Users = mongoose.model('Users', Schema);
-
-mongoose.connect(url, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
+const monk = require('monk')
+const db = monk('mongodb://heroku_1rfb6cx7:csft257r52q2mqtq41fdpt2dg6@ds129641.mlab.com:29641/heroku_1rfb6cx7')
+//let url = 'mongodb://heroku_1rfb6cx7:csft257r52q2mqtq41fdpt2dg6@ds129641.mlab.com:29641/heroku_1rfb6cx7'
 
 function textParser(text, identifier) {
   return text.split(' ').filter((element) => {
@@ -25,37 +12,65 @@ function textParser(text, identifier) {
   })
 }
 
+let errorMessage = {
+  response_type: 'ephemeral', // private message (only visible by user).
+  text: 'Error: ',
+  attachments:[
+    {
+      text: "There was a problem adding the information to the database."
+    }
+  ]
+};
+
+
 express()
   .use(bodyParser.json()) // support json encoded bodies
   .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
+  // Make our db accessible to our router
+  .use(function(req, res, next){
+    req.db = db;
+    next();
+  })
+
   .post('/', function (req, res) {
-    let todo = new Users( req.body );
+    // set internal DB variable
+    let db = req.db
 
-    let user = textParser(req.body.text, '@');
+    let userName = textParser(req.body.text, '@');
     let task = textParser(req.body.text, '#');
+    let timestamp = Date.now();
+    let available = false;
 
-    todo.id = todo._id;
-    todo.user = user;
-    todo.task = task;
-    todo.timestamp = Date.now();
-    todo.available = false;
-    let data = {
-      response_type: 'in_channel', // private message (only visible by user).
-      text: 'How to use /httpstatus command:',
-      attachments:[
-        {
-          text: todo.user + " was assigned to " + todo.task
-        }
-      ]
-    };
-    todo.save(function (err) {
-      res.json(data);
-    });
-    console.log(todo)
+    // Set collection
+    let collection = db.get('usercollection')
+
+    // Submit to the DB
+    collection.insert({
+      "username": userName,
+      "task": task,
+      "timestamp": timestamp,
+      "available": available
+    }, (err, doc) => {
+      if (err) {
+        // If it failed, return error
+        res.json(errorMessage);
+      } else {
+        // set up response message
+        let data = {
+          response_type: 'in_channel', // private message (only visible by user).
+          text: 'Successful pull request assigntment:',
+          attachments:[
+            {
+              text: userName + " was assigned to pull request " + task
+            }
+          ]
+        };
+        res.json(data);
+      }
+    })
   })
 
   .listen(process.env.PORT || 5000, () => {
     console.log('Expresss server listening on port ' + process.env.PORT || 5000)
   });
-
