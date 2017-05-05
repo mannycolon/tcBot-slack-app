7f213e85dde94d8206d2716a645a8b5ac562868e
mannycolon <colonmanuel7@gmail.com>
2017-05-05 03:00:51 -0400

    again

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ express()
     // Set collection
     let collection = db.get('usercollection')
     //finding to see if there is a document in the collection with the userName.
-    let docFound = collection.find({username: userName})
+    let docFound = collection.find({username: userName}).explain()
     console.log(docFound)
     // Submit to the DB
     collection.insert({
