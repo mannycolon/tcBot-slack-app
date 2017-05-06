f57e278753ba443c91b40fefe1ab80d1ebab4cf1
mannycolon <colonmanuel7@gmail.com>
2017-05-06 19:14:05 -0400

    created switch to handle the multiple differnetion action types coming in from gh webhooks when a pr is either created, opened, closed, reopened or when assinged or unnassigned to someone

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const monk = require('monk')
 const db = monk('mongodb://heroku_1rfb6cx7:csft257r52q2mqtq41fdpt2dg6@ds129641.mlab.com:29641/heroku_1rfb6cx7')
+// request handler functions
+import { handleOpenPR, handleAssignPR, handleUnassignPR} from "./requestHandlers"
 
 function textParser(text, identifier) {
   return text.split(' ').filter((element) => {
@@ -36,36 +38,27 @@ express()
     // set internal DB variable
     let db = req.db
 
-    //opened / closed / reopened / first pr creation/open (req.body.action)
-    // req.body.action
-    // req.body.sender.login
-    // req.body.pull_request.number
-    // req.body.pull_request.url
-    // req.body.pull_request.assignees (array)
-
-    // assigned / unassigned (req.body.action)
-    // pr requester
-    // req.body.sender.login
-    // reviewer assignee
-    // req.body.assignee.login
-    // req.body.pull_request.number
-    // req.body.pull_request.url
-    
-    //review_request_removed (req.body.action)
-    //review_requested (req.body.action)
-    // pr requester
-    // req.body.sender.login
-    //req.body.pull_request.number
-    //req.body.pull_request.url
-    //req.body.requested_reviewer.login
-
-    // note: when you first open a pr and dont assignit to anyone then
-    // req.body.action = opened
-    // and req.body.pull_request.assignees = []
-    /****
-     * however when you first open/create a pr and do asign it to someone
-     * then req.body.action = assigned and req.body.pull_request.assignees = ['userobject', 'userobject']
-     */
+    switch (req.body.action) {
+      case "opened":
+      case "reopened":
+        console.log(req.body.action)
+        handleOpenedPR(req, res, db)
+        break
+      case "assigned":
+        console.log(req.body.action)
+        handleAssignedPR(req, res, db)
+        break
+      case "unassigned":
+        console.log(req.body.action)
+        handleUnassignedPR(req, res, db)
+        break
+      case "closed":
+        console.log(req.body.action)
+        handleClosedPR(req, res, db)
+      default:
+        // do nothing
+        break
+    }
 
     // https://hooks.slack.com/commands/T0TH52
   })
