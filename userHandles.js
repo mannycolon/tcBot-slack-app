3ec857d0d5638e04776840119ad229b74a9e2644
mannycolon <colonmanuel7@gmail.com>
2017-05-07 04:25:55 -0400

    created user handles object file to mathc user handles from tc slack and tc github repos

---
+++ userHandles.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mannycolon: '@mannycolon',
+  royalsix: '@royalsix',
+  cdwhitfield73: '@cdwhitfield73',
+  ihoegen: '@ihoegen',
+  klappy: '@klappy',
+  EllDoubleYew: '@EllDoubleYew'
+}
