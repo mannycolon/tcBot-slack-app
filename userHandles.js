009ee7fa99adb783ee1f0943007deb68ff98e869
mannycolon <colonmanuel7@gmail.com>
2017-05-08 13:03:34 -0400

    fixed bug with jays user handles

--- userHandles.js
+++ userHandles.js
@@ -1,6 +1,7 @@
 module.exports = {
   mannycolon: '@mannycolon',
   RoyalSix: '@royalsix',
+  royalSix: '@royalsix',
   cdwhitfield73: '@cdwhitfield73',
   ihoegen: '@ihoegen',
   klappy: '@klappy',
