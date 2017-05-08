37a84bdc427c3223ed554e3a563977726117aff7
mannycolon <colonmanuel7@gmail.com>
2017-05-07 21:51:42 -0400

    moved comments/info not needed in index.js to index2.js for future reference

--- index2.js
+++ index2.js
@@ -93,3 +93,41 @@ express()
   .listen(process.env.PORT || 5000, () => {
     console.log('Expresss server listening on port ' + process.env.PORT || 5000)
   });
+
+
+//opened / closed / reopened / first pr creation/open (req.body.action)
+    // req.body.action
+    // req.body.sender.login
+    // req.body.pull_request.number)
+    // req.body.pull_request.html_url
+    // req.body.pull_request.assignees (array)
+
+    // assigned / unassigned (req.body.action)
+    // pr requester
+    // req.body.sender.login
+    // reviewer assignee
+    // req.body.assignee.login
+    // req.body.pull_request.number
+    // req.body.pull_request.html_url
+    
+    //review_request_removed (req.body.action)
+    //review_requested (req.body.action)
+    // pr requester
+    // req.body.sender.login
+    //req.body.pull_request.number
+    //req.body.pull_request.html_url
+    //req.body.requested_reviewer.login
+
+    // merge and closed seem to be the same.
+
+    // note: when you first open a pr and dont assign it to anyone then
+    // req.body.action = opened
+    // and req.body.pull_request.assignees = []
+    /****
+     * however when you first open/create a pr and do asign it to someone
+     * then req.body.action = assigned and req.body.pull_request.assignees = ['userobject', 'userobject']
+     */
+
+
+    // pull_request.head.repo.name
+    //https://reviewable.io/reviews/translationcoreapps/translationhelps/30
