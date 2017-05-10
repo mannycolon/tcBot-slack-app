5a477f75a2bcee5975e6a507511056f50a34549c
mannycolon <colonmanuel7@gmail.com>
2017-05-09 23:52:15 -0400

    fixed bug

--- requestHandlers.js
+++ requestHandlers.js
@@ -22,7 +22,7 @@ function handleTaskAssignment(req, res, db) {
     let userName = userHandles[assignee.login]
     //finding to see if there is a document in the collection with the userName.
     if (userName) {
-      collection.ensureIndex({userName: 1}, {unique: true})
+      collection.ensureIndex({username: 1}, {unique: true})
       collection.find({ username: userName}).then((docFound) => {
         if (docFound.length === 0) {
           // Submit to the DB
