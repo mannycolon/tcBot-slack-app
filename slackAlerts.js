3c5068e416a8191b0e11702c3c455b074a8b248e
mannycolon <colonmanuel7@gmail.com>
2017-05-10 01:58:52 -0400

    improved slack alert/notifiation msg

--- slackAlerts.js
+++ slackAlerts.js
@@ -2,36 +2,51 @@ const MY_SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/T0TH52342/B5AHX4F
 const slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
 
 function taskAssignmentSlackAlert(originator, assigneeAdded, repoName, taskURL, taskNumber, fullRepoName, action) {
-  slack.alert({
-    text: "Successful Pull Request Assignment:",
-    attachments: [
-      {
-        text: originator + " " + action + " a *Pull Request.*",
-        mrkdwn_in: ["text", "pretext"],
-        color: "#36a64f",
-        title: repoName,
-        title_link: taskURL,
-        fields: [
-          {
-            title: "Assigned to",
-            value: assigneeAdded,
-            short: true
-          },
-          {
-            title: "Pull Request Number",
-            value: "#" + taskNumber,
-            short: true
-          },
-          {
-            title: "Pull Request URL",
-            value: taskURL + "\n\n\n <https://reviewable.io/reviews/"
-                    + fullRepoName + "/" + taskNumber + "|Review Now>",
-            short: false
-          }
-        ]
-      }
-    ]
-  });
+  if (action === "assigned") {
+    slack.alert({
+      text: "Successful Pull Request Assignment:",
+      attachments: [
+        {
+          text: originator + " " + action + " *Pull Request* " + "<" + taskURL + "|" + "#"
+                + taskNumber + ">" + " " + " to " + assigneeAdded,
+          mrkdwn_in: ["text", "pretext"],
+          color: "#439fe0",
+          title: repoName,
+          title_link: taskURL,
+          fields: [
+            {
+              title: "Pull Request URL",
+              value: taskURL + "\n\n\n <https://reviewable.io/reviews/"
+                      + fullRepoName + "/" + taskNumber + "|Review Now>",
+              short: false
+            }
+          ]
+        }
+      ]
+    });
+  }else {
+    slack.alert({
+      text: "Successful Pull Request Assignment:",
+      attachments: [
+        {
+          text: originator + " " + action + " *Pull Request* " + "<" + taskURL + "|" + "#"
+                + taskNumber + ">" + " and assigned it to " + assigneeAdded,
+          mrkdwn_in: ["text", "pretext"],
+          color: "#439fe0",
+          title: repoName,
+          title_link: taskURL,
+          fields: [
+            {
+              title: "Pull Request URL",
+              value: taskURL + "\n\n\n <https://reviewable.io/reviews/"
+                      + fullRepoName + "/" + taskNumber + "|Review Now>",
+              short: false
+            }
+          ]
+        }
+      ]
+    });
+  }
 }
 
 function taskUnassignmentSlackAlert(originator, assigneeRemoved, repoName, taskURL, taskNumber) {
